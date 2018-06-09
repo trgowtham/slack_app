c0cd9d0df410c4d2847362b310ebea848ddde0ca
Nirmal Agarwal <forevernirmal@gmail.com>
2018-06-09 16:22:14 +0100

    db_utils: main() modified to list stocks in DB.

--- db_utils.py
+++ db_utils.py
@@ -34,23 +34,10 @@ def create_db():
     mydb.close()
 
 if __name__ == '__main__':
-    'create db and check '
-    create_db()
-    import sys;sys.exit(0)
-    # #check and print
+
+    # print all the stocks in DB
     mydb = MyZODB()
-    print(mydb.dbroot.items())
-    st = Stock('name', 'type', 'symbol', 'reco_date', 'reco_date_price', 'risk_score')
-    st1 = Stock('name', 'type', 'symbol1', 'reco_date', 'reco_date_price', 'risk_score')
     db_stock = mydb.dbroot.stocks
-    for stock in [st, st1]:
-        if stock.symbol not in db_stock:
-            db_stock[stock.symbol] = stock
-        else:
-            print(f'Not adding {st.symbol}')
-    transaction.commit()
-    print(mydb.dbroot)
-    print(type(mydb.dbroot.stocks))
     print(f'items in db {len(mydb.dbroot.stocks.items())}')
     for item in mydb.dbroot.stocks:
         print (item)
