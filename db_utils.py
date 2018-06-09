a3ece3e10d06fcf2d180b26af7cabeb6ff912648
Nirmal Agarwal <forevernirmal@gmail.com>
2018-06-09 16:54:40 +0100

    db_utils.py: Singleton DB instance across

--- db_utils.py
+++ db_utils.py
@@ -2,7 +2,8 @@ import os
 import ZODB, ZODB.FileStorage
 import BTrees.OOBTree
 import transaction
-
+import logging
+from logging.config import fileConfig
 from stocks import Stock
 
 DB_PATH= os.path.dirname(os.path.realpath(__file__)) + '/db/mydata.fs'
@@ -12,16 +13,28 @@ import transaction
 
 
 class MyZODB(object):
+
+    # singleton DB instance
+    _instance = None
+
+    def __new__(cls):
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls)
+        return cls._instance
+
     def __init__(self):
+        # return if connection already set
+        if  hasattr(self, "connection"):
+            logging.debug(f'DB Object already exists. Return')
+            return
+        logging.debug('Creating a new db instance')
         self.storage = FileStorage.FileStorage(DB_PATH)
         self.db = DB(self.storage)
         self.connection = self.db.open()
         self.dbroot = self.connection.root()
 
     def close(self):
-        self.connection.close()
         self.db.close()
-        self.storage.close()
 
 def create_db():
     #One time action of creating and DB and other actions
@@ -34,11 +47,13 @@ def create_db():
     mydb.close()
 
 if __name__ == '__main__':
-
+    fileConfig('logging.ini', disable_existing_loggers=True)
+    logger = logging.getLogger()
     # print all the stocks in DB
     mydb = MyZODB()
     db_stock = mydb.dbroot.stocks
     print(f'items in db {len(mydb.dbroot.stocks.items())}')
     for item in mydb.dbroot.stocks:
         print (item)
+
     mydb.close()
