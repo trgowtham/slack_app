e292f0f22ad3ee66e3b60be15a746c92c93c60fc
System Administrator <root@gt.local>
2018-07-16 15:33:03 +0530

    Modify slack_cron.sh to support zeo DB

--- slack_cron.sh
+++ slack_cron.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-pgrep python3.6
+pgrep -af python3.6 | grep slack.py > /dev/null
 
 if [ $? -eq 1 ]
 then
-	pkill -9 python3.6
-	cd /home/ubuntu/slack_app && /home/ubuntu/slack_app/myvenv/bin/python3.6 /home/ubuntu/slack_app/slack.py > /home/ubuntu/slack_app/slack_log 2>&1 &
+        pkill -9 python3.6
+        pkill -9 runzeo
+        cd /home/ubuntu/slack_app
+        source /home/ubuntu/slack_app/myvenv/bin/activate
+        python3.6 /home/ubuntu/slack_app/slack.py > /home/ubuntu/slack_app/slack_log 2>&1 &
 fi
